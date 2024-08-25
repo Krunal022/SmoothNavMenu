ab47f49c9cdadd66b5a8b510cee9108132617f85
Krunal022 <krunalvaghela723@gmail.com>
2024-08-25 11:37:54 +0530

    smooth open/close toggle for enhanced user experience.

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
-gsap.from("h1",{
-    x:100,
-    duration:20,
-    opacity:0,
-    color:"blue"
+var menu = document.querySelector("nav i")
+var cross = document.querySelector("#full i")
+
+var tl = gsap.timeline()
+
+tl.to("#full", {
+    right: 0,
+    duration: .6,
+})
+tl.from("#full h4", {
+    x: 150,
+    duration: 0.7,
+    opacity: 0,
+    stagger: .27
+})
+tl.from("#full i", {
+    opacity: 0,
+    duration: .5
+})
+tl.pause()
+
+menu.addEventListener("click", function () {
+    tl.play()
+})
+cross.addEventListener("click", function () {
+    tl.reverse()
+
+    // tl.to("#full",{
+    //     x:300,
+    //     opacity:0,
+    //     duration:.8
+    // })    
 })
